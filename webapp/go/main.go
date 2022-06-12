8653a8270876e674db48a8dd5ffe26edfe2e1ab4
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2022-06-12 18:39:08 +0900

    fix check isu be

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1211,7 +1211,6 @@ func postIsuCondition(c echo.Context) error {
 	} else if len(req) == 0 {
 		return c.String(http.StatusBadRequest, "bad request body")
 	}
-
 	tx, err := db.Beginx()
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
@@ -1219,13 +1218,17 @@ func postIsuCondition(c echo.Context) error {
 	}
 	defer tx.Rollback()
 
-	var count int
-	err = tx.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_isu_uuid` = ?", jiaIsuUUID)
+	//var count int
+	//err = tx.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_isu_uuid` = ?", jiaIsuUUID)
+	isuList := []Isu{}
+	err = tx.Get(&isuList, "SELECT jia_isu_uuid FROM `isu` WHERE `jia_isu_uuid` = ? LIMIT 1", jiaIsuUUID)
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
-	if count == 0 {
+
+	//if count == 0 {
+	if len(isuList) == 0 {
 		return c.String(http.StatusNotFound, "not found: isu")
 	}
 
